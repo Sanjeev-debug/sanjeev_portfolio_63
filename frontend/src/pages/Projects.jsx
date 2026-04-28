874c9c8639278274a4fef24b5859209ec02567d9
Sanjeev <ms.saisanjeev@gmail.com>
2026-04-28 12:06:45 +0530

    import change

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -15,7 +15,7 @@ import RestaurantWeb from '../assets/RestaurantWeb.mp4'
 import lovevisiteWeb from '../assets/lovevisite.mp4'
 import AI_Assistant from '../assets/AI_Assistant.mp4'
 import YouTube_Analyzer from '../assets/YouTube_Analyzer.mp4'
-import smartClassAttend from '../assets/smartClassAttend.mp4'
+import smartClassAttend from '../assets/smartClassattend.mp4'
 
 import HaatPoster from '../assets/poster.png'
 import CarPoster from '../assets/CarMoving.png'
